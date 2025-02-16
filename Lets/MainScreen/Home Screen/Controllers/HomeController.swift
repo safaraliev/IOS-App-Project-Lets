addad67148b5c633d61f7abc60892fe92a721ae4
safaraliev <nurikonurik@gmail.com>
2025-02-16 02:33:02 -0600

    saturday update, finished Welcome and Auth Screens, started Home Screen UI, TabBar)

---
+++ Lets/MainScreen/Home Screen/Controllers/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeController.swift
+//  Lets
+//
+//  Created by Нуридин Сафаралиев on 2/15/25.
+//
+
+import UIKit
+
+class HomeController: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
